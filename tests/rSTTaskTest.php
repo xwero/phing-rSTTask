4865f85a501053dc002364cbe8169f27dbe48f93
Christian Weiske <cweiske@cweiske.de>
2011-06-12 20:06:37 +0200

    more tests

--- tests/rSTTaskTest.php
+++ tests/rSTTaskTest.php
@@ -8,31 +8,59 @@ class rSTTaskTest extends BuildFileTest
         $this->configureProject(
             PHING_TEST_BASE . '/build.xml'
         );
+        //$this->assertInLogs('Property ${version} => 1.0.1');
+    }
+
+
+
+    /**
+     * Checks if a given file has been created and unlinks it afterwards.
+     *
+     * @param string $file relative file path
+     *
+     * @return void
+     */
+    protected function assertFileCreated($file)
+    {
+        $this->assertTrue(
+            file_exists(dirname(__FILE__) . '/' . $file),
+            $file . ' has not been created'
+        );
+        unlink(dirname(__FILE__) . '/' . $file);
     }
 
 
     public function testSingleFileParameterFile()
     {
         $this->executeTarget(__FUNCTION__);
-        $this->assertTrue(
-            file_exists(dirname(__FILE__) . '/single.html'),
-            'single.html has not been created'
-        );
-        unlink(dirname(__FILE__) . '/single.html');
-        //$this->assertInLogs('Property ${version} => 1.0.1');
+        $this->assertFileCreated('single.html');
     }
 
     public function testSingleFileParameterFileNoExt()
     {
         $this->executeTarget(__FUNCTION__);
-        $this->assertTrue(
-            file_exists(dirname(__FILE__) . '/single-no-ext.html'),
-            'single-no-ext.html has not been created'
-        );
-        unlink(dirname(__FILE__) . '/single-no-ext.html');
-        //$this->assertInLogs('Property ${version} => 1.0.1');
+        $this->assertFileCreated('single-no-ext.html');
     }
 
+    public function testSingleFileParameterFileFormat()
+    {
+        $this->executeTarget(__FUNCTION__);
+        $this->assertFileCreated('single.3');
+    }
+
+    public function testSingleFileInvalidParameterFormat()
+    {
+        $this->expectBuildExceptionContaining(
+            __FUNCTION__, 'Invalid parameter',
+            'Invalid output format "foo", allowed are'
+        );
+    }
+
+    public function testSingleFileParameterFileFormatTarget()
+    {
+        $this->executeTarget(__FUNCTION__);
+        $this->assertFileCreated('single-target.html');
+    }
 }
 
 ?>
